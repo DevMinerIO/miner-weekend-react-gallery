d2d359c00dbd2203b58283f3382876adb1a9a278
Tim Miner <poeminminer1@gmail.com>
2022-07-09 22:26:52 -0500

    onClick function working to toggel between having a description and not.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 
 function GalleryItem({ item }) {
+    const [isToggled, setIsToggled] = useState(false);
+
+    const handleClick = () => {
+        console.log('you clicked a picture!');
+        setIsToggled(!isToggled);
+    }
     
     return (
         <>
-            <div className="single-photo">
-                <img src={item.path} />
+            {/* Attempt at creating a boolean to check if the img is clicked.
+            on click show the description */}
+            {isToggled ? <div className="item-description">
+                <img onClick={handleClick} src={item.path} />
                 <p>{item.description}</p>
-                <br />
                 <button>I love it!</button>
                 <p><span>{item.likes}</span> People loved this!</p>
-            </div>
+            </div> :
+                <div className="single-photo">
+                    <img onClick={handleClick} src={item.path} />
+                    <br />
+                    <button>I love it!</button>
+                    <p><span>{item.likes}</span> People loved this!</p>
+                </div>
+            }
         </>
     )
 }
