7bd5ac761c19f3aac9af6f4b4eca70728280f8ef
Tim Miner <poeminminer1@gmail.com>
2022-07-09 20:36:51 -0500

    Get function is working properly

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+// import { response } from 'express';
 
 function App() {
+
+  // state to hold gallery photos
+  const [photoList, setPhotoList] = useState([]);
+
+  const getPhotos = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+      .then((response) => {
+        console.log('Response in APP:', response);
+        console.log('the data from App:', response.data);
+        // setting state for photoList
+        setPhotoList(response.data);
+      })
+      .catch(function (error) {
+        console.log('ERROR ON GET, in APP', error);
+      });
+  }
+  // on ready function to load gallery when page is loaded. done once
+  useEffect( () => {
+    getPhotos();
+  }, [])
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
+        
       </div>
     );
 }
